9b96c3c9f900d4e559c5e5f74db5397965a7ec26
yilaoshi704 <3194221715@qq.com>
2024-01-23 22:32:59 +0800

    code

--- code/1.19石头剪刀布.py
+++ code/1.19石头剪刀布.py
@@ -8,8 +8,8 @@ class RockPaperScissors:
     def get_user_choice(self):
         while True:
             choice = input("请选择：剪刀，石头，布：")
-            if choice.lower() in self.choices:
-                return choice.lower()
+            if choice in self.choices:
+                return choice
             else:
                 print("无效选择，请重新选择。")
 
@@ -19,9 +19,9 @@ class RockPaperScissors:
     def determine(self, user_choice, computer_choice):
         if user_choice == computer_choice:
             return "平局！"
-        elif (user_choice == "rock" and computer_choice == "scissors") or (
-                user_choice == "scissors" and computer_choice == "paper") or (
-                user_choice == "paper" and computer_choice == "rock"):
+        elif (user_choice == "石头" and computer_choice == "剪刀") or (
+                user_choice == "剪刀" and computer_choice == "布") or (
+                user_choice == "布" and computer_choice == "石头"):
             return "牛啊牛啊！"
         else:
             return "拉了拉了！"
@@ -34,11 +34,15 @@ class RockPaperScissors:
             print("电脑选择：", computer_choice)
             print(self.determine(user_choice, computer_choice))
 
-            play_again = input("再来一轮？(y/n) ")
-            if play_again.lower() != "y":
-                game_over = True
+            while True:
+                play_again = input("再来一轮？(y/n) ")
+                if play_again == "y":
+                    break
+                elif play_again.lower() == "n":
+                    game_over = True
+                    break
+                else:
+                    print("输入有误！！")
 
-
-# 实例化游戏对象，并启动游戏
 game = RockPaperScissors()
 game.play()
