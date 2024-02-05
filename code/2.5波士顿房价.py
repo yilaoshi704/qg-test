e32c792fb8aa5b239cd068f3072cc841db2572b4
yilaoshi704 <3194221715@qq.com>
2024-02-06 00:33:09 +0800

    code

---
+++ code/2.5波士顿房价.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+def load_data():
+    data = np.genfromtxt('boston.csv', delimiter=',')
+    # print(data)
+
+    # 数据处理
+    # 分组
+    rows = data.shape[0]
+    columns = data.shape[1]
+    # print(rows)
+    # print(columns)
+    data = data.reshape([rows, columns])
+    # print(data)
+
+    # 划分数据集 80%训练集 20%测试集
+    offset = int(data.shape[0] * 0.8)
+    training_data = data[:offset]
+
+    # 计算train数据集的最大值，最小值，平均值
+    maximums, minimums, avgs = \
+        training_data.max(axis=0), \
+            training_data.min(axis=0), \
+            training_data.sum(axis=0) / training_data.shape[0]
+
+    # 数据归一
+    for i in range(14):
+        # print(maximums[i], minimums[i], avgs[i])
+        data[:, i] = (data[:, i] - avgs[i]) / (maximums[i] - minimums[i])
+    training_data = data[:offset]
+    test_data = data[offset:]
+    return training_data, test_data
+
+
+# 构建神经网络
+class Network(object):
+    def __init__(self, num_of_weights):
+        np.random.seed(0)
+        self.w = np.random.randn(num_of_weights, 1)
+        self.b = 0.
+
+    # 前向计算
+    def forward(self, x):
+        z = np.dot(x, self.w) + self.b
+        return z
+
+    # 计算损失率
+    def loss(self, z, y):
+        cost = (z - y) * (z - y)
+        cost = np.sum(cost) / (z - y).shape[0]
+        return cost
+
+    # 梯度计算
+    def gradient(self, x, y):
+        z = self.forward(x)
+        gradient_w = (z - y) * x
+        gradient_w = np.mean(gradient_w, axis=0)
+        gradient_w = gradient_w[:, np.newaxis]
+        gradient_b = (z - y)
+        gradient_b = np.mean(gradient_b)
+        return gradient_w, gradient_b
+
+    # 下降
+    def update(self, gradient_w, gradient_b, eta=0.01):
+        self.w = self.w - eta * gradient_w
+        self.b = self.b - eta * gradient_b
+
+    # 训练
+    def train(self, x, y, iterations=100, eta=0.01):
+        losses = []
+        for i in range(iterations):
+            z = self.forward(x)
+            L = self.loss(z, y)
+            gradient_w, gradient_b = self.gradient(x, y)
+            self.update(gradient_w, gradient_b, eta)
+            losses.append(L)
+            if (i + 1) % 1000 == 0:
+                print('iter {}, loss {}'.format(i, L))
+        return losses
+
+    # 保存
+    def save_model(self, save_path):
+        model = {'w': self.w, 'b': self.b}
+        np.save(save_path, model)
+        print("Model saved successfully!")
+
+
+# 获取数据
+train_data, test_data = load_data()
+x = train_data[:, :-1]
+y = train_data[:, -1:]
+# 创建网络
+net = Network(13)
+num_iterations = 10000
+# 启动训练
+losses = net.train(x, y, iterations=num_iterations, eta=0.01)
+net.save_model("model")
+
+# 画出损失函数的变化趋势
+# 汉化字体
+matplotlib.rc("font", family='DengXian')
+plt.figure(figsize=(8, 8), num="损失函数")
+plot_x = np.arange(num_iterations)
+plot_y = np.array(losses)
+plt.plot(plot_x, plot_y)
+plt.ylabel("损失率", fontsize=15)
+plt.title("损失率变化图", fontsize=25)
+
+plt.show()
